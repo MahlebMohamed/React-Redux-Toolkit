4b4281f5b616e793754d81d9ed4a83407e82d593
MahlebMohamed <mohamed_mahleb@hotmail.com>
2023-11-16 10:18:17 +0100

    01 lesson

---
+++ 01_lesson/src/App.js
@@ -0,0 +1,16 @@
+// import Counter from "./features/counter/Counter";
+
+import AddPostForm from "./features/posts/AddPostForm";
+import PostsList from "./features/posts/PostsList";
+
+function App() {
+  return (
+    <main className="App">
+      {/* <Counter /> */}
+      <AddPostForm />
+      <PostsList />
+    </main>
+  );
+}
+
+export default App;
